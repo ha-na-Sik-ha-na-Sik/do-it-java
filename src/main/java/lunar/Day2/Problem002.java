67ca4252f43b43bcf8935a3223eb93aa89e217fc
wyoung163 <20201014@sungshin.ac.kr>
2023-03-12 20:36:47 +0900

    성공: 자료구조(배열과 리스트) - 평균 구하기

---
+++ src/main/java/lunar/Day2/Problem002.java
@@ -0,0 +1,25 @@
+package lunar.Day2;
+
+import java.io.*;
+import java.util.*;
+
+//배열
+public class Problem002 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double[] num = new double[n];
+        for(int i = 0; i < n; i++){
+            num[i] = Double.parseDouble(st.nextToken());
+        }
+
+        Arrays.sort(num);
+        double answer = 0;
+        for(int i = 0; i < n; i++){
+            answer += num[i]/num[n-1]*100;
+        }
+
+        System.out.println(answer/n);
+    }
+}
