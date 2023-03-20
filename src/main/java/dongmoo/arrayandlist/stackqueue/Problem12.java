e25ac90eb4fe89529be3ed37c759fd336717b4e2
Limjongho <whdgh9873@icloud.com>
2023-03-20 15:41:58 +0900

    문제 번호 수정

--- src/main/java/dongmoo/arrayandlist/stackqueue/Problem12.java
+++ src/main/java/dongmoo/arrayandlist/stackqueue/Problem12.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-public class Problem11 {
+public class Problem12 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
