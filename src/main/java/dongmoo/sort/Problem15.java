3a275810b7ca29bd958a1ef151195427e11c08b6
Limjongho <whdgh9873@icloud.com>
2023-03-25 15:12:03 +0900

    성공 : 버블정렬 - 수 정렬하기1

---
+++ src/main/java/dongmoo/sort/Problem15.java
@@ -0,0 +1,29 @@
+package dongmoo.sort;
+
+import java.util.Scanner;
+
+public class Problem15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N - i -1 ; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i : A) {
+            System.out.println(i);
+        }
+    }
+}
