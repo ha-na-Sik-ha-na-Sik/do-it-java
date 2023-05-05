176f38521edbd15f52cb0b83fec91a963d983711
Limjongho <whdgh9873@icloud.com>
2023-05-05 19:43:25 +0900

    실패(시간초과) : 그리디 - 카드정렬하기

--- src/main/java/dongmoo/greedy/Problem33.java
+++ src/main/java/dongmoo/greedy/Problem33.java
@@ -1,29 +1,31 @@
 package dongmoo.greedy;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Problem33 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = Integer.parseInt(sc.nextLine());
-        int[] A = new int[N];
+        List<Integer> A = new ArrayList<>();
         int answer = 0;
+        int seq = 0;
 
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(sc.nextLine());
+            A.add(Integer.parseInt(sc.nextLine()));
         }
 
-        Arrays.sort(A);
-
         if (N == 1) {
-            answer = A[0];
+            answer = 0;
         } else if (N == 2) {
-            answer = A[0] + A[1];
+            answer = A.get(0) + A.get(1);
         } else {
-            answer += A[0] * (N - 1);
-            for (int i = 1; i < N; i++) {
-                answer += (N - i) * A[i];
+            while (A.size() > 1) {
+                Collections.sort(A);
+                int i = A.get(0) + A.get(1);
+                A.remove(0);
+                A.remove(0);
+                answer += i;
+                A.add(i);
             }
         }
 
