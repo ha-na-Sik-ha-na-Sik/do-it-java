c5d89bc9eeb01799e6fb959755363e43a6be377d
Limjongho <whdgh9873@icloud.com>
2023-05-23 20:51:49 +0900

    실패(알고리즘 이해 실패): 그래프 - 물의 양 구하기

---
+++ src/main/java/dongmoo/graph/Problem49.java
@@ -0,0 +1,15 @@
+package dongmoo.graph;
+
+import java.util.Scanner;
+
+// 물의 양 구하기
+public class Problem49 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+
+    }
+}
