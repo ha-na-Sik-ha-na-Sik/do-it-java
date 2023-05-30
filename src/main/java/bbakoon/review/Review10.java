7d1866553055bf1029617777abafece42d620e76
HunSeongPark <phunseong@gmail.com>
2023-05-31 03:46:19 +0900

    복습: sliding_window - 최솟값 찾기

---
+++ src/main/java/bbakoon/review/Review10.java
@@ -0,0 +1,33 @@
+package bbakoon.review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Review10 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        Deque<int[]> deq = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            int n = Integer.parseInt(st.nextToken());
+            while (!deq.isEmpty() && deq.getLast()[0] >= n) {
+                deq.removeLast();
+            }
+            if (!deq.isEmpty() && deq.getFirst()[1] < i - L + 1) {
+                deq.removeFirst();
+            }
+            deq.addLast(new int[]{n, i});
+            answer.append(deq.getFirst()[0]).append(" ");
+        }
+        System.out.println(answer);
+    }
+}
